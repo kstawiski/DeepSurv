27ad671447b12e2feb4279ae512553b5cc8dd528
Konrad Stawiski <konrad@konsta.com.pl>
2020-07-09 19:52:35 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install git+https://github.com/anthonytw/downsample.git
+pip install git+https://github.com/Lasagne/Lasagne.git
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+pip install .
